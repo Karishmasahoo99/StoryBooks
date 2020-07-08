7ebece4c68a27d5fde2e0a3f56798756eef372be
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-08 11:29:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+const path=require('path');
 const express=require("express");
 const dotenv= require('dotenv');
+const morgan=require('morgan');
+const exphbs = require('express-handlebars');
 const connectdb=require('./config/db');
 
-//Load config
-dotenv.config({path: './config/config'});
-
-connectdb()
+connectdb();
 
 const app=express();
+
+//Logging
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main',extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routes
+app.use('/',require('./routes/index'));
+
+//Static folder
+app.use(express.static(path.join(__dirname+'/public')))
+
 const PORT= process.env.PORT|| 5000;
 
-app.listen(PORT,console.log("Server running in "+ process.env.NODE_ENV +" mode on port: "+ PORT));
+console.log(process.env.MONGO_URI);
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
