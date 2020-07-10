1808cf12316566caa9b012e3136ccc94fd26f438
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-10 11:15:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express=require("express");
 const dotenv= require('dotenv');
 const morgan=require('morgan');
 const passport=require('passport');
+const session=require('express-session');
 const exphbs = require('express-handlebars');
 const connectdb=require('./config/db');
 
@@ -13,22 +14,36 @@ connectdb();
 
 const app=express();
 
-//Logging
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'))
-}
-
 //Handlebars
 app.engine('.hbs', exphbs({defaultLayout:'main',extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-//Routes
-app.use('/',require('./routes/index'));
+//session middleware
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 //Static folder
 app.use(express.static(path.join(__dirname+'/public')))
 
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/auth',require('./routes/auth'));
+
 const PORT= process.env.PORT|| 5000;
 
-console.log(process.env.MONGO_URI);
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
