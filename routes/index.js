ace555c2a61204ef6eb60c30b4ce2f84deabe61b
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-11 11:24:46 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const { ensureAuth, ensureGuest}=require('../middleware/auth');
+const Story=require('../models/Story')
 
 router.get("/",(req,res)=>{
     res.render('login',{
