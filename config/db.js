bdf1588f8f4327685fa02d36e17f5ee217192d57
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-12 20:04:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,16 @@
-const mongoose=require('mongoose');
-require('dotenv').config({path: './config/config'});
-
-const connectdb= async()=>{
+const mongoose=require('mongoose')
+const connectDb= async()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
+        const conn=await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useFindAndModify:false
         })
-        console.log("MongoDb connected "+ conn.connection.host)
+        console.log(`Mongodb connected: ${conn.connection.host}`)
     }
     catch(err){
         console.log(err)
         process.exit(1)
     }
 }
-
-module.exports= connectdb;
+module.exports= connectDb
