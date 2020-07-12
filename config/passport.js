3c772d5a1373f3d867213b609abd8c24fd471772
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-12 20:02:12 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,45 +1,42 @@
-const GoogleStrategy= require("passport-google-oauth20").Strategy;
-const mongoose=require('mongoose');
-const User=require('../models/User');
-const GOOGLE_CLIENT_ID=require('./config').GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET=require('./config').GOOGLE_CLIENT_SECRET;
+const GoogleStrategy=require('passport-google-oauth20').Strategy
+const mongoose=require('mongoose')
+const User=require('../models/User')
 
 module.exports=function(passport){
-    passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/google/callback"
-      },
-      async(accessToken, refreshToken, profile,done)=> {
-        const newUser={
-          googleId:profile.id,
-          displayName:profile.displayName,
-          firstName:profile.name.givenName,
-          lastName:profile.name.familyName,
-          image:profile.photos[0].value
-        }
-        try{
-          let user=await User.find({googleId: profile.id})
-
-          if(user){
-            done(null,user)
-          }
-          else{
-            user=await User.create(newUser)
-            done(null,user)
-          }
-        }
-        catch(err){
-            console.log(err)
-        }
-      }
-    ));
-
-    passport.serializeUser((user, done)=> {
-        done(null, user.id);
-    });
-       
-    passport.deserializeUser((id, done)=> {
-      User.findById(id,(err, user)=> done(err, user));
-   });
+   passport.use(new GoogleStrategy({
+       clientID:process.env.GOOGLE_CLIENT_ID,
+       clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+       callbackURL:'http://localhost:3000/auth/google/callback'
+   },
+   async(accessToken,refreshToken,profile,done)=>{
+       const newUser={
+           googleId:profile.id,
+           displayName:profile.displayName,
+           firstName:profile.name.givenName,
+           lastName:profile.name.familyName,
+           image:profile.photos[0].value,
+       }
+       try{
+           let user=await User.findOne({"req.user.googleId":profile.id})
+           if(user){
+               return done(null,user.id)
+           }
+           else{
+               user=await User.create(newUser)
+               return done(null,user)
+           }
+       }
+       catch(err){
+           console.log(err)
+       }
+    }
+   ))
+   passport.serializeUser((user,done)=>{
+       done(null,user.id)
+   })
+   passport.deserializeUser((id,done)=>{
+       User.findById(id,(err,user)=>{
+           done(err,user)
+       })
+   })
 }
