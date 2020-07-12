8fb821144205b815b9008c19b42ab2627ccf1cda
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-12 20:03:02 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
 module.exports={
-    ensureAuth:function(req,res,next){
+    ensureAuth: function(req,res,next){
         if(req.isAuthenticated()){
-            return next();
+            return next()
         }
         else{
-            res.redirect('/');
+            res.redirect('/')
         }
     },
     ensureGuest:function(req,res,next){
