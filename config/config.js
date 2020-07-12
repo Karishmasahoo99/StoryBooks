045219282cf06760dcf455929d92707cbc174aae
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-12 20:00:48 +0530

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-PORT=3000;
-MONGO_URI="mongodb+srv://dbUser:dbUser@stories.f2sue.mongodb.net/STORYBOOKS?retryWrites=true&w=majority";
-GOOGLE_CLIENT_ID="urgoogleclientid";
-GOOGLE_CLIENT_SECRET="urgoogleclientsecret";
-
-module.exports.PORT=PORT;
-module.exports.MONGO_URI=MONGO_URI;
-module.exports.GOOGLE_CLIENT_ID=GOOGLE_CLIENT_ID;
-module.exports.GOOGLE_CLIENT_SECRET=GOOGLE_CLIENT_SECRET;
