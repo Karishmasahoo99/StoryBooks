dfcd73970196eda94ae2b4de7a2c912c0515ec91
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-12 20:05:54 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,17 @@
-const express=require('express');
-const passport = require('passport');
-const router=express.Router();
+const express=require('express')
+const passport = require('passport')
+const router=express.Router()
 
-//Google authentication
+router.get('/google',passport.authenticate('google',{scope:['profile']}))
 
-router.get("/google",passport.authenticate('google',{scope:['profile']}))
-
-//Google auth callback
-router.get("/google/callback",passport.authenticate('google', { failureRedirect:'/'
-}),(req,res)=>{
-    res.redirect('/dashboard')
-}
+router.get('/google/callback',passport.authenticate('google',{failureRedirect:'/'}),
+   (req,res)=>{
+       res.redirect('/dashboard')
+   }
 )
 
-//Logout
 router.get('/logout',(req,res)=>{
-    res.logout()
+    req.logout()
     res.redirect('/')
 })
 
