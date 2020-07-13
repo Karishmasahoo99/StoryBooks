c13e20801b2cafd1ca696cfda0f47d8d6081d101
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-13 23:01:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ require('./config/passport')(passport)
 connectDb()
 
 const app=express()
+
+//Body Parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
@@ -53,6 +58,7 @@ app.use(express.static(path.join(__dirname,'public')))
 //Routes
 app.use('/',require('./routes/index'))
 app.use('/auth',require('./routes/auth'))
+app.use('/stories',require('./routes/stories'))
 
 const PORT=process.env.PORT ||5000;
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
